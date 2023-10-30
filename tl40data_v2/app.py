510bd3bb4e07554ddd20077611df37d97feed754
Eric Relson <gertlex@gmail.com>
2023-10-29 21:59:53 -0700

    Various app.py updates from past several months
    - Warning text of unsatisfactory entries
    - Order of text box per-user for ease of entry

--- tl40data_v2/app.py
+++ tl40data_v2/app.py
@@ -11,7 +11,8 @@ import sys
 from flask import request, flash, redirect, url_for, send_from_directory
 from flask import Flask
 from flask import render_template
-from wtforms import Form, BooleanField, DecimalField, StringField, IntegerField, PasswordField, validators
+from wtforms import Form, BooleanField, DecimalField, StringField, IntegerField, \
+                    PasswordField, validators
 from flask_wtf import FlaskForm
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
@@ -25,9 +26,26 @@ from settings import LOCAL_DB_SPECIFIER
 
 
 STATS = []
-PogoForm = None
-
-session = None
+MEDALS = ["No medal", "Bronze", "Silver", "Gold", "Platinum"]
+TYPE_MEDALS = ["Schoolkid",  # Used for offsets
+               "Black Belt",
+               "Bird Keeper",
+               "Punk Girl",
+               "Ruin Maniac",
+               "Hiker",
+               "Bug Catcher",
+               "Hex Maniac",
+               "Rail Staff",
+               "Kindler",
+               "Swimmer",
+               "Gardener",
+               "Rocker",
+               "Psychic",
+               "Skier",
+               "Dragon Tamer",
+               "Delinquent",
+               "Fairy Tale Girl",
+              ]
 
 app = Flask(__name__)
 
@@ -40,8 +58,9 @@ def get_survey_data_in_survey_order(session, user=None):
         user (str): trainer name
         stats: Preloaded information about stat categories and badge thresholds.
             Otherwise query DB for this information
+
     Returns:
-        stats: (TODO rename?) list of lists. Each sublist is
+        stats_list: (TODO rename?) list of lists. Each sublist is
             [Name, img, previous value/None, maximum/None]
     """
     # We get two lists, the stats_list from json
@@ -110,36 +129,63 @@ def get_survey_data_in_survey_order(session, user=None):
         #    #print("SKIPPING:", x[4])
         #    continue  # Skip some things that aren't in the survey, like Alola and Hisui dexes
         try:
-            previousval = trainer_data[key]
+            # The actual keys are tuples...
+            previousval = trainer_data[(key,)]
+            if previousval == '':  # Trying to load a new field that is not in the previous response's strdata
+                previousval = 0
+            else:
+                previousval = int(previousval) # TODO int or float support
+        except ValueError as e:
+            previousval = float(trainer_data[(key,)])  # Technically we could do this always
         except KeyError:
+            print("Missing key:", key)
+            previousval = 0
+        except TypeError:
+            #print(f"TypeError with trainer_data[{key}], {type(key)}")
             previousval = 0
 
+        # Calculate order offsets used for ordering of survey items
         bronze_thresh = x[1]
-        if bronze_thresh == 0:  # Always put these level-less stats at top of render order
-            # TODO revisit and start from 0, rather than using negative numbers
-            #order = -400 + cnt
-            order_offset = -400
-        else:  # All other categories, lower orders for higher badge numbers, displayed first
-            #order = cnt
+        if bronze_thresh == 0:  # 0 indicates NO MEDALS for this stat
+            # Always put these medal-less stats at top of render order
             order_offset = 0
-            medal_idx = 1  # 1/2/3/4 bronze/silver/gold/platinum
-            while medal_idx < 5 and previousval > x[medal_idx]:
-                #order -= 100
+            medal_name = ""
+        else:  # All other categories; lower orders for higher badge numbers, displayed first
+            # TODO(enhancement): If a state is zero, set the order_offset even higher...
+            order_offset = 400
+            medal_idx = 1  # 1/2/3/4/5 none/bronze/silver/gold/platinum
+            while medal_idx < 5 and previousval >= x[medal_idx]:
                 order_offset -= 100
                 medal_idx += 1
+            medal_name = f"({MEDALS[medal_idx-1]})"
+
+            # Special cases
+            if key == "Vivillon Collector":
+                order_offset = 499  # hardcode this one to be last before type medals
+            elif key in TYPE_MEDALS:
+                # Put these after the other medals
+                order_offset += 600
 
         # From CSS stuff, for reference...
         #print(f"    #main-panel .mdl-grid > #{x[4]} " + "{ order: " + f"{order}; " + "}")
         stats_list[cnt][0] += order_offset
-        stats_list[cnt][2] = previousval
+        if medal_name:
+            stats_list[cnt][2] = f"{previousval} {medal_name}"
+        else:
+            stats_list[cnt][2] = f"{previousval}"
 
     # Sort things!
+    stats_list.sort(key=lambda x: x[0])
 
     # Returns:
     # stats: (TODO rename?) list of lists. Each sublist is
     #     [Name, img, previous value/None, maximum/None]
     return stats_list
 
+
+def print_incomplete_warning():
+    # Return text (HTML) that will be displayed above the Submit button
+    return "<p><b>There are one or more empty required fields, or errors to correct.</b>"
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -160,8 +206,10 @@ class PogoStatsForm(Form):
     pass
 
 
-def survey_gen(stats_list, formclass):
+def survey_gen(stats_list, formclass, _test_default_val=None):
     """Sets survey fields as attrs on form object
+
+    Also inserts sectional breaks.
 
     Args:
         stats_list: list of Stat objects. Each list will drive a input field on the form.
@@ -172,35 +220,64 @@ def survey_gen(stats_list, formclass):
         A class with attributes set, ready to be instantiated.
     """
     statlist = []  # lookup of attribute names on the FormClass which hold Field objects
+    statorder = []  # list of booleans
+    # TODO(enhancement) this is stupidly brittle? Every time I add a new non-badge stat...? or if we get too many badges
+    # See also shenanigans around line 150 above.
+    sections = [20, 100, 200, 300, 400, 500, 600, 700, 800, 900, 10000]
+    section_idx = 0
     for order, stat, previous_val in stats_list:  # in order already
-        # TODO
-        placeholder = 123
+        # 
+        print(section_idx, order, stat.icon)
+        if order > sections[section_idx]:
+            statorder.append(True)
+            while order > sections[section_idx]:
+                section_idx += 1
+        else:
+            statorder.append(False)
+
+        print(stat.name, previous_val)
         if stat.name == 'Trainer Level':
-            minimum = 40
+            minimum = max(40, int(previous_val))
+        elif stat.monotonic:
+            # We're passing in "123 (badge level)" so need to strip second part before casting
+            try:
+                minimum = int(previous_val.split()[0])  # todo floats
+            except:
+                minimum = 0
         else:
             minimum = 0
         checks = [validators.NumberRange(min=minimum,
-                                             max=stat.maximum if stat.maximum > 0 else None)
+                                         max=stat.maximum if stat.maximum > 0 else None)
                      ]
         if not stat.required:
             checks = [validators.Optional()] + checks
         if stat.numtype == "Float":
             # Per wtforms docs, DecimalField is usually preferred over FloatField
             field = DecimalField(stat.name, validators=checks,
-                                 render_kw={"inputmode": "numeric", "type": "number"},
+                                 default=_test_default_val,
+                                 render_kw={"inputmode": "numeric", "type": "number",
+                                            "placeholder": previous_val,
+                                            "previous_val_with_badge": previous_val,  # unused
+                                            },
                                  )
         else:
             field = IntegerField(stat.name, validators=checks,
                                  # TODO move this image part to a setatrr call?
                                  #image="imgs/" + stat.icon + ".png",  # accessed with statlist[i].kwargs['image']
+                                 default=_test_default_val, # Could use this but it fills a valid value. Use render_kw["placeholder"] instead
                                  render_kw={"inputmode": "numeric", "type": "number",
-                                     "placeholder": previous_val},
+                                            "placeholder": previous_val,
+                                            "previous_val_with_badge": previous_val,  # unused
+                                            },
                                  )
-        setattr(formclass, stat.icon, field)  # using icon because name has spaces in it
-        #statlist.append(field)  # Problem is this is the unbound field, DUH
-        #statlist.append(getattr(formclass, stat.icon))
+        # Set the field object on our form, which will later generate the HTML
+        # We cast str on stat.icon, otherwise we actually have a sqlalchemy object, and this causes
+        # thread issues with sqlite at render time.  (This theory didn't prove correct)
+        setattr(formclass, str(stat.icon), field)  # using icon because name has spaces in it
+        # Order of newly added attrs is preserved in statlist
         statlist.append(stat.icon)
     setattr(formclass, "statlist", statlist)
+    setattr(formclass, "statorder", statorder)
     # https://gaming.stackexchange.com/a/281007
     trainername = StringField('Trainer Name',
                               validators=[validators.Length(min=4, max=15),
@@ -223,12 +300,14 @@ def printx(*args):
 
 @app.route("/<month>")
 def stats_previous(month=None):
+    """month is something like 2022-3"""
     # Should load the previous month's stats
     return send_from_directory('static', month)
 
 # Not sure this is necessary
 @app.route("/static/<month>")
 def stats_previous_from_static(month=None):
+    """month is something like 2022-3"""
     # Should load the previous month's stats
     return send_from_directory('static', month)
 
@@ -238,6 +317,7 @@ def stats():
     return send_from_directory('static', 'index.html')
 
 
+# TODO not implemented
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     print()
@@ -259,13 +339,16 @@ def register():
         flash('Thanks for registering')
 
         return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    return render_template('register.html', form=form,
+                           zip=zip, type=type, print=print,
+                           )
 
 @app.route('/survey/<username>', methods=['GET', 'POST'])
 def fill_survey_for_user(username=None):
     return fill_survey(username)
 
 @app.route('/survey', methods=['GET', 'POST'])
+@app.route('/survey/', methods=['GET', 'POST'])
 def fill_survey(user=None):
     # Fill in the data for the form generation
     #global STATS
@@ -274,21 +357,127 @@ def fill_survey(user=None):
     #stats_list = STATS
 
     # Generate a stats list, either default order, or order by user's badge levels if known
-    global session
+    session = Session(engine, autoflush=True)
     stats_list = get_survey_data_in_survey_order(session=session, user=user)
 
-    global PogoForm
-    if PogoForm is None:
-        # This will return TODO
-        PogoForm = survey_gen(stats_list, PogoStatsForm)
-    form = PogoForm(request.form)
+    PogoForm = survey_gen(stats_list, PogoStatsForm)
+    try:
+        form = PogoForm(request.form)
+        real_function_call = True  # TODO cleanup
+        # These are debug stuff...
+        print("")
+        if hasattr(request, "values"):
+            print(request.values)
+    except RuntimeError:  # likely "Working outside of request context"
+        # Presumably this is because we're testing stuff and request isn't defined.
+        form = PogoForm()
+        real_function_call = False  # TODO cleanup
+
+    # Prefill trainername field
+    if user:
+        form.trainername.data = user
+    else: # add a button that routes to /survey/<username> using the tranername field
+        pass
+        # TODO
+        # Button
+
+
+    html_out = "placeholder"
+    print("", file=sys.stderr)
+    try:
+        print("DEBUG HERE - got", request.method)
+        if hasattr(request, "values"):
+            print(request.values)
+        request_method = request.method
+        form_validated = form.validate()
+    except RuntimeError:
+        # e.g. app.py was run with --test-get-survey-data or similar option
+        request_method = 'POST'
+        form_validated = True
+    if request_method == 'POST' and form_validated:
+        # If valid
+            # Display validation success
+            # Save in DB
+            # Display save success
+            # Redirect to user history page
+        if session:  # is set up
+            response = Response.save_response(session, response_values=request.values)
+            print(response)
+            session.commit()
+            session.flush()
+
+            # Submission page
+            # <pre> tags preserve the tab characters, so users can paste data into spreadsheets
+            html_out = "Thanks for the submission! Stats will be regenerated on the 1st.<p>" \
+                    "But here's the raw data you submitted if you want to back it up for now:<p>" \
+                    "<pre>" \
+                    + "<br>".join([f"{k}:\t{v}" for k, v in request.values.items()]) \
+                    + "</pre>"
+        else:
+            print("skipped db_session.add call")
+        #flash('Thanks for ...')
+
+        # TODO subsequent HTML or redirect
+
+    else:
+        #print("DEBUG HERE - got", request.method)
+        print("RENDER HERE:")
+
+        # Print a helpful warning if there are errors in the survey (i.e. validation failed)
+        # otherwise, no-op.
+        if request_method == 'POST' and not form_validated:
+            print("THERE WERE INCOMPLETE STATS:")
+            piw = print_incomplete_warning
+        else:
+            piw = lambda: ""
+
+        try:
+            html_out = render_template('survey_template.html', form=form,
+                                       zip=zip, type=type, print=print,
+                                       print_incomplete_warning=piw,
+                                       )
+        except RuntimeError:
+            pass
+
+    try:
+        session.close()
+    except:
+        pass
+
+    return html_out
+
+
+@app.route('/test_survey/', methods=['GET', 'POST'])
+def fill_test_survey():
+    # Fill in the data for the form generation
+    #global STATS
+    #if not STATS:  # TODO find a main() method or whatever convention flask uses and run this here...
+    #    get_survey_data_in_survey_order(None, user=user)
+    #stats_list = STATS
+
+    # Generate a stats list, either default order, or order by user's badge levels if known
+    session = Session(engine, autoflush=True)
+    stats_list = get_survey_data_in_survey_order(session=session, user="test")
+
+    PogoForm = survey_gen(stats_list, PogoStatsForm)
+    try:
+        form = PogoForm(request.form)
+        real_function_call = True  # TODO cleanup
+        # These are debug stuff...
+        print("")
+        if hasattr(request, "values"):
+            print(request.values)
+    except RuntimeError:  # likely "Working outside of request context"
+        # Presumably this is because we're testing stuff and request isn't defined.
+        form = PogoForm()
+        real_function_call = False  # TODO cleanup
 
     # Prefill trainername field
     if user:
         form.trainername.data = user
 
     html_out = "placeholder"
-    print("")
+    print("", file=sys.stderr)
     print("DEBUG HERE - got", request.method)
     if hasattr(request, "values"):
         print(request.values)
@@ -298,7 +487,6 @@ def fill_survey(user=None):
             # Save in DB
             # Display save success
             # Redirect to user history page
-        #global session  # Delete this; I do it above; 2-26
         if session:  # is set up
             response = Response.save_response(session, response_values=request.values)
             print(response)
@@ -321,7 +509,21 @@ def fill_survey(user=None):
     else:
         print("DEBUG HERE - got", request.method)
         print("RENDER HERE:")
-        html_out = render_template('survey_template.html', form=form)
+        if request_method == 'POST' and form_validated:
+            piw = print_incomplete_warning
+        else:  # GET
+            piw = lambda: ""
+
+        html_out = render_template('survey_template.html', form=form,
+                                   zip=zip, type=type, print=print,
+                                   print_incomplete_warning=piw,
+                                   )
+
+    try:
+        session.close()
+    except:
+        pass
+
     return html_out
 
 
@@ -332,12 +534,15 @@ if __name__ == '__main__':
     parser.add_argument("--test-user", action='store', default=None)
     args = parser.parse_args()
 
-    db_specifier = LOCAL_DB_SPECIFIER
+    if True: # Later can support alternate
+        db_specifier = LOCAL_DB_SPECIFIER
+        print(f"Using: {LOCAL_DB_SPECIFIER}")
+
     engine = create_engine(db_specifier)
-    session = Session(engine, autoflush=True)
 
     if args.test_get_survey_data:
-        print("TESTING get_survey_data_in_survey_order")
+        # Why autoflush?
+        session = Session(engine, autoflush=True)
         get_survey_data_in_survey_order(session, user=args.test_user)
         exit(0)
 
