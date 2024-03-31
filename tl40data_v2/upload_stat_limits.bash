18f4af7a9ff0cfec8a88c7fa44fd6e4850750a09
Eric Relson <gertlex@gmail.com>
2024-03-30 22:09:20 -0700

    Git enhancements to upload_stat_limits.bash

--- tl40data_v2/upload_stat_limits.bash
+++ tl40data_v2/upload_stat_limits.bash
@@ -1,9 +1,31 @@
-# Read config.toml; just need the login line
+# Read config.toml; parsing is simple because we only need the login line
 login=$(grep login config.toml | cut -d' ' -f3 | tr -d '"')
 
-echo $login
+echo "From config, will log into server as: $login"
 
-# Ask if upload new html (and which)
+# Only check for differences in stats.json, but commit changes to stat_max_notes.md too.
+if [[ -n "$(git diff stats.json)" ]]; then
+  echo "Now reviewing changes:"
+  git diff stats.json stat_max_notes.md
+  
+  echo "Do you want to commit those changes? (y/n)"
+  read -r answer
+  if [[ "$answer" = "y" ]]; then
+      git add stats.json stat_max_notes.md
+      git commit
+  else
+      echo "Do you want to abort? (y/n)"
+      read -r answer
+      if [[ "$answer" = "y" ]]; then
+          echo Not uploading. Exiting.
+          exit 0
+      fi
+  fi
+#else
+#    # No changes for git but we still do upload
+fi
+
+# Ask if upload updated stats.json
 echo "Do you want to upload the latest stats.json? (y/n)"
 read -r answer
 if [[ "$answer" != "y" ]]; then
